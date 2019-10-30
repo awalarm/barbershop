439eab0364a67ee5945d9e0f9bf040af1a3159bf
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-10-30 11:17:31 +0400

    add post user in txt

--- app.rb
+++ app.rb
@@ -12,3 +12,17 @@ end
 get '/visit' do
   erb :visit
 end
+
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+
+  f = File.open("./public/users.txt", "a")
+  f.write "Имя клиента: #{@username},Телефон: #{@phone}\n"
+  f.close
+
+  @message = "#{@username}, мы перезвонил вам для уточнения времени по телефону #{@phone}."
+  erb :message
+end
+
+
